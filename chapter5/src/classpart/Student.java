75800623b9d9fe537b5e2f971146f32cfb238a43
나 <@DESKTOP-QA0M42J>
2021-07-24 18:44:00 +0900

    abc

---
+++ chapter5/src/classpart/Student.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class Student {
+
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void studentInfo() {
+	 System.out.println(studentName + "," + address);
+	}
+}
---
+++ chapter5/src/classpart/StudentTest.java
@@ -0,0 +1,14 @@
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student();
+		studentLee.studentName = "순신";
+		studentLee.address = "경기도 안양";
+				
+		studentLee.studentInfo();
+	}
+
+}
